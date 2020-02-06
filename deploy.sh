32247e1234e56a9c1f68de5fe1aabc5f44101550
christian-nils <christian-nils.boda@gadz.org>
2020-02-06 11:53:36 +0100

    update the deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,17 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+rm -rf public
+
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+
 
 # Go To Public folder
 cd public
 # Add changes to git.
-git add .
+git init
+git add -A
 
 # Commit changes.
 msg="rebuilding site `date`"
@@ -17,8 +21,9 @@ if [ $# -eq 1 ]
 fi
 git commit -m "$msg"
 
-# Push source and build repos.
-git push origin master
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git remote add origin https://github.com/ruvigroup/ruvigroup.github.io.git
+git push -f origin master
 
 # Come Back up to the Project Root
 cd ..
